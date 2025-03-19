43beebcb1946f4aa68f5f2d66cce8ae0370c45ef
Matteo Manià <matteomania05@gmail.com>
2025-03-19 18:27:50 +0100

    crivello di eratostene

--- esercizi_java/Esercizi_programmazione_2025/tutto.java
+++ esercizi_java/Esercizi_programmazione_2025/tutto.java
@@ -89,4 +89,71 @@ public class tutto {
         }
         return u[n];
     }
+    
+    // ufo --- Soluzione "Top down" ---
+    /*
+     * n = 13
+     * |12| /2 |6| /2 |3| /2 |1|
+     *                        1
+     *                 3
+     *          5
+     *  9
+     *
+     *  k:
+     *  y = (1,3) 5
+     *  T[] a = T[ dim ];  // creazione di un array
+     *  a[i] = ...         // accesso in scrittura
+     *  ... = ... a[i] ... // accesso in lettura
+     *  if/while (... a[i] ...)
+     */
+    public static int ufoTopDown ( int n ) {
+        int[] x = new int[ (int) (Math.log(n)/Math.log(2)) + 1 ];   // log in base 2 di n
+        x[0] = n;   // (limite sup) prima invocazione ricorsiva (argomento n)
+        
+        // fase discendente - ritorno con i risultati
+        int k = 0;
+        while( x[k] > 1 ) {   // se caso base > 1
+            x[k+1] = x[k] / 2;   // es: 12/2 = 6, ... , 1.
+            k = k + 1;          // indice
+        }
+        
+        // fase ascendente - chiamate ricorsive
+        int y = 1;  // x = 1
+        while( k > 0 ) {
+            k = k - 1;
+            if ( x[k] % 2 == 0 ) {
+                y = 2 * y - 1;
+            } else {
+                y = 2 * y + 1;
+            }
+        }
+        
+        return y;
+    }
+    
+    // Crivello di Eratostene
+    public static void eratostene( int n ) {
+        //System.out.print( "  2" );
+        int count = 1;
+        
+        boolean[] crivello = new boolean[ n/2+1 ];
+        for ( int k=3; k<=n; k=k+2 ) {
+            crivello[k/2] = true;
+        }
+        
+        for ( int k=3; k<=n; k=k+2 ) {
+            if ( crivello[k/2] ) {
+                //System.out.print( "  " + k );
+                count = count + 1;
+                long m = ((long) k)*k;
+                while( m <= n ) {
+                    crivello[ (int) m/2] = false;
+                    m = m + 2*k; 
+                }
+            }
+        }
+        System.out.println( "  P(" + n + ") = " + count );
+        System.out.println( "  R(" + n + ") = " + (n/Math.log(n)) );
+        System.out.println( "  P(" + n + ")/R(" + n + ") = " + (count/(n/Math.log(n))) );
+    }
 }
