f616863e0dd136ca86b5626c01ae7530eafe330b
Matteo Manià <matteomania05@gmail.com>
2025-04-28 11:44:48 +0200

    llcs,lcs dinamici

--- esercizi_java/Esercizi_programmazione_2025/Memoization.java
+++ esercizi_java/Esercizi_programmazione_2025/Memoization.java
@@ -98,6 +98,115 @@ public class Memoization {
         return mem[i][j];
     }
     
+    
+    /**
+     * lcs (differente da llcs), non ricorsivo
+     */
+    
+    public static String lcs(String u, String v) {
+        int m = u.length(); 
+        int n = v.length(); 
+        
+        String[][] mem = new String[m+1][n+1]; 
+        for (int i = 0; i<=m; i++) {
+            for (int j = 0; j<=n; j++) {
+                mem[i][j] = null;
+            }
+        }
+        
+        return lcsRec(u,v,mem);
+    }
+    
+    public static String lcsRec(String u, String v, String[][] mem) {
+        int i = u.length(); 
+        int j = v.length(); 
+        
+        if(mem[i][j] == null) {
+            if((i == 0) || (j == 0)) {
+               mem[i][j] = "";  
+            } else if (u.charAt(0) == v.charAt(0)) {
+                mem[i][j] = u.substring(0,1) + lcsRec(u.substring(1),v.substring(1),mem);
+            } else {
+                mem[i][j] = longer(lcsRec(u.substring(1),v,mem),
+                                   lcsRec(u,v.substring(1),mem) ); 
+            }
+        }
+        return mem[i][j];
+    }
+    
+    private static String longer (String u, String v) {
+        int m = u.length(); 
+        int n = v.length(); 
+        
+        if(m < n) {
+            return v; 
+        } else if (m > n) {
+            return u; 
+        } else if (Math.random() < 0.5) {   // probabilità che è sempre diversa (50%)
+            return v;
+        } else {
+            return u; 
+        }
+        
+    }
+    
+    /**
+     * lcs con programmazione dinamica (computazione efficiente)
+     */
+    public static String lcsDP(String u, String v) {
+        int m = u.length();
+        int n = v.length();
+        
+        int[][] mem = new int[m+1][n+1];
+        
+        // Inizializzazione prima riga
+        for (int j = 0; j <= n; j = j + 1) {
+            mem[0][j] = 0;
+        }
+        
+        // Inizializzazione prima colonna
+        for (int i = 1; i <= m; i = i + 1) {
+            mem[i][0] = 0;
+        }
+        
+        // Riempimento tabella DP
+        for (int i = 1; i <= m; i = i + 1) {
+            for (int j = 1; j <= n; j = j + 1) {
+                if (u.charAt(m - i) == v.charAt(n - j)) {
+                    mem[i][j] = 1 + mem[i - 1][j - 1];
+                } else {
+                    mem[i][j] = Math.max(mem[i - 1][j], mem[i][j - 1]);
+                }
+            }
+        }
+        
+        String s = "";
+        int i = m;
+        int j = n;
+        
+        while (mem[i][j] > 0) {
+            if (u.charAt(m - i) == v.charAt(n - j)) {
+                s = s + u.charAt(m - i);
+                i = i - 1;
+                j = j - 1;
+            } 
+            else if (mem[i - 1][j] < mem[i][j - 1]) {
+                j = j - 1;
+            } 
+            else if (mem[i - 1][j] > mem[i][j - 1]) {
+                i = i - 1;
+            } 
+            else if (Math.random() < 0.5) {
+                j = j - 1;
+            } 
+            else {
+                i = i - 1;
+            }
+        }
+        
+        return s;
+    }
+    
     public static long manhDP(int i, int j) {
         
         long[][] mem = new long[i+1][j+1]; 
